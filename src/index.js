7146fe6d47cbe4e90271137bd6c2e8764eddeb6f
Alex Leshenko <alex.l@sca.auction>
2020-02-20 14:07:05 +0300

    complited task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = towelSort = (matrix) =>{
+    let arrFinal = [];
+    if (matrix){
+        matrix.forEach(function (item, i) {
+            if (i % 2 !== 0) {
+                arrFinal.push(...item.reverse())
+            } else {
+                arrFinal.push(...item)
+            }
+        });
+    }
+    return arrFinal;
 }
